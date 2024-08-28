6c7946fb26a7c728ab5a35c33394ee17cae728cb
이정곤 <jungggon@ijeong-gon-ui-MacBookPro.local>
2024-08-28 10:47:41 +0900

    과제 수정

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -8,29 +8,23 @@ function Dex() {
   const [selectedPokemon, setSelectedPokemon] = useState(defaultValue);
 
   const addPokemon = (pokemon) => {
-    if (selectedPokemon.indexOf) {
+    if (selectedPokemon.length > 6) {
+      alert("포켓몬은 최대 6개까지만 선택 될 수 있어요.");
+    } else if (selectedPokemon.includes(pokemon)) {
       alert("이미 선택된 포켓몬 입니다!");
-      return;
+    } else {
+      setSelectedPokemon([...selectedPokemon, pokemon]);
     }
-    const updatedList = [...selectedPokemon, pokemon];
-    if (updatedList.length > 5) {
-      alert("최대 6개의 포켓몬만 선택할 수 있습니다!");
-      return;
-    }
-    setSelectedPokemon(updatedList);
+    return (
+      <div>
+        <Dashboard
+          selectedPokemon={selectedPokemon}
+          onRemovePokemon={removePokemon}
+        />
+        <PokemonList pokemonList={MOCK_DATA} onAddPokemon={addPokemon} />
+      </div>
+    );
   };
-
-  const removePokemon = (pokemon) => {};
-
-  return (
-    <div>
-      <Dashboard
-        selectedPokemon={selectedPokemon}
-        onRemovePokemon={removePokemon}
-      />
-      <PokemonList pokemonList={MOCK_DATA} onAddPokemon={addPokemon} />
-    </div>
-  );
 }
 
 export default Dex;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
     <div>
-        <img src=""></img>
-        <div>
-            <h1>포켓몬 도감</h1>
-            <button onClick={() => navigate('/dex')}>
-            포켓몬 도감 시작하기</button>
-            <button onClick={() => navigate('/Testpages')}>
-            관리자 테스트</button>
-        </div>
+      <div>
+        <h1>포켓몬 도감</h1>
+        <button onClick={() => navigate("/dex")}>포켓몬 도감 시작하기</button>
+      </div>
     </div>
   );
 }
